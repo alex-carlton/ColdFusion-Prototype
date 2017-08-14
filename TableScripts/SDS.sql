e47d369833e1ea4c40b57a2f88213f62e1d7d426
Kullen Williams <kullen@auburn.edu>
2017-08-14 14:27:31 -0400

    initial secure data store DB and Table create scripts
    still needs seed and service account set up

---
+++ TableScripts/SDS.sql
@@ -0,0 +1,218 @@
+USE [master]
+GO
+
+/****** Object:  Database [SecureDataStore]    Script Date: 8/14/2017 1:08:56 PM ******/
+DROP DATABASE [SecureDataStore]
+GO
+
+/****** Object:  Database [SecureDataStore]    Script Date: 8/14/2017 1:08:56 PM ******/
+CREATE DATABASE [SecureDataStore]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'SecureDataStore', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\SecureDataStore.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'SecureDataStore_log', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\SecureDataStore_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+ALTER DATABASE [SecureDataStore] SET COMPATIBILITY_LEVEL = 130
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [SecureDataStore].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+-- ALTER DATABASE [SecureDataStore] SET ANSI_NULL_DEFAULT OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET ANSI_NULLS OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET ANSI_PADDING OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET ANSI_WARNINGS OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET ARITHABORT OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET AUTO_CLOSE OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET AUTO_SHRINK OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET AUTO_UPDATE_STATISTICS ON 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET CURSOR_CLOSE_ON_COMMIT OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET CURSOR_DEFAULT  GLOBAL 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET CONCAT_NULL_YIELDS_NULL OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET NUMERIC_ROUNDABORT OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET QUOTED_IDENTIFIER OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET RECURSIVE_TRIGGERS OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET  DISABLE_BROKER 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET DATE_CORRELATION_OPTIMIZATION OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET TRUSTWORTHY OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET PARAMETERIZATION SIMPLE 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET READ_COMMITTED_SNAPSHOT OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET HONOR_BROKER_PRIORITY OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET RECOVERY FULL 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET  MULTI_USER 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET PAGE_VERIFY CHECKSUM  
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET DB_CHAINING OFF 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET DELAYED_DURABILITY = DISABLED 
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET QUERY_STORE = OFF
+-- GO
+
+ USE [SecureDataStore]
+ GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
+-- GO
+
+-- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
+-- GO
+
+-- ALTER DATABASE [SecureDataStore] SET  READ_WRITE 
+-- GO
+
+-- *********************************************************************************
+-- Create Pii Text Table
+-- *********************************************************************************
+CREATE TABLE [dbo].[PiiText](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Data] [varchar](100) NOT NULL,
+	[TimeStamp] [timestamp] NOT NULL,
+	[Type] [varchar](50) NOT NULL,)
+GO
+
+-- Create Pii Text indexes
+CREATE UNIQUE NONCLUSTERED INDEX [dataCluster] ON [dbo].[PiiText]
+(
+	[Data] ASC,
+	[Type] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [idCluster] ON [dbo].[PiiText]
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
+-- *********************************************************************************
+-- Create Pii Date Table
+-- *********************************************************************************
+CREATE TABLE [dbo].[PiiDate](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Data] [date] NOT NULL,
+	[TimeStamp] [timestamp] NOT NULL,
+	[Type] [varchar](50) NULL,)
+GO
+
+-- Create Pii Date indexes
+CREATE UNIQUE NONCLUSTERED INDEX [dataCluster] ON [dbo].[PiiDate]
+(
+	[Data] ASC,
+	[Type] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+CREATE UNIQUE CLUSTERED INDEX [idCluster] ON [dbo].[PiiDate]
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
+-- *********************************************************************************
+-- Create Pii DComplex ate Table
+-- *********************************************************************************
+CREATE TABLE [dbo].[PiiComplex](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Data] [varchar(-1)] NOT NULL,
+	[TimeStamp] [timestamp] NOT NULL,
+	[Type] [varchar](50) NULL,)
+GO
+
+-- -- Create Pii Date indexes
+-- CREATE UNIQUE NONCLUSTERED INDEX [dataCluster] ON [dbo].[PiiComplex]
+-- (
+-- 	[Data] ASC,
+-- 	[Type] ASC
+-- )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+-- GO
+
+CREATE UNIQUE CLUSTERED INDEX [idCluster] ON [dbo].[PiiComplex]
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
