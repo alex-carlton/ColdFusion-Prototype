0ec60d4f347a6f28a2535338162173cd057fa278
Kullen Williams <kullen@auburn.edu>
2017-08-14 15:55:04 -0400

    Table Name update, Added if exists drop DB, changed type column from varchar to int

--- TableScripts/ADS.sql
+++ TableScripts/ADS.sql
@@ -2,116 +2,16 @@ USE [master]
 GO
 
 /****** Object:  Database [ApplicationDataStore]    Script Date: 8/14/2017 2:35:57 PM ******/
+USE [master]
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ApplicationDataStore')
 DROP DATABASE [ApplicationDataStore]
 GO
 
 /****** Object:  Database [ApplicationDataStore]    Script Date: 8/14/2017 2:35:57 PM ******/
 CREATE DATABASE [ApplicationDataStore]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'ApplicationDataStore', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\ApplicationDataStore.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'ApplicationDataStore_log', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\ApplicationDataStore_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
 GO
 
--- ALTER DATABASE [ApplicationDataStore] SET COMPATIBILITY_LEVEL = 130
--- GO
 
--- IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
--- begin
--- EXEC [ApplicationDataStore].[dbo].[sp_fulltext_database] @action = 'enable'
--- end
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ANSI_NULL_DEFAULT OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ANSI_NULLS OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ANSI_PADDING OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ANSI_WARNINGS OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ARITHABORT OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET AUTO_CLOSE OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET AUTO_SHRINK OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET AUTO_UPDATE_STATISTICS ON 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET CURSOR_CLOSE_ON_COMMIT OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET CURSOR_DEFAULT  GLOBAL 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET CONCAT_NULL_YIELDS_NULL OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET NUMERIC_ROUNDABORT OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET QUOTED_IDENTIFIER OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET RECURSIVE_TRIGGERS OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET  DISABLE_BROKER 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET DATE_CORRELATION_OPTIMIZATION OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET TRUSTWORTHY OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET ALLOW_SNAPSHOT_ISOLATION OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET PARAMETERIZATION SIMPLE 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET READ_COMMITTED_SNAPSHOT OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET HONOR_BROKER_PRIORITY OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET RECOVERY FULL 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET  MULTI_USER 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET PAGE_VERIFY CHECKSUM  
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET DB_CHAINING OFF 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET TARGET_RECOVERY_TIME = 60 SECONDS 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET DELAYED_DURABILITY = DISABLED 
--- GO
-
--- ALTER DATABASE [ApplicationDataStore] SET QUERY_STORE = OFF
--- GO
 
 USE [ApplicationDataStore]
 GO
@@ -208,3 +108,8 @@ CREATE TABLE [dbo].[CreditCheck](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+
+
+
+USE [master]
--- TableScripts/SDS.sql
+++ TableScripts/SDS.sql
@@ -2,155 +2,30 @@ USE [master]
 GO
 
 /****** Object:  Database [SecureDataStore]    Script Date: 8/14/2017 1:08:56 PM ******/
+USE [master]
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ApplicationDataStore')
 DROP DATABASE [SecureDataStore]
 GO
 
 /****** Object:  Database [SecureDataStore]    Script Date: 8/14/2017 1:08:56 PM ******/
 CREATE DATABASE [SecureDataStore]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'SecureDataStore', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\SecureDataStore.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'SecureDataStore_log', FILENAME = N'C:\Program Files\Microsoft SQL Server 16\MSSQL13.MSSQLSERVER01\MSSQL\DATA\SecureDataStore_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
 GO
 
--- ALTER DATABASE [SecureDataStore] SET COMPATIBILITY_LEVEL = 130
--- GO
 
--- IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
--- begin
--- EXEC [SecureDataStore].[dbo].[sp_fulltext_database] @action = 'enable'
--- end
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ANSI_NULL_DEFAULT OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ANSI_NULLS OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ANSI_PADDING OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ANSI_WARNINGS OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ARITHABORT OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET AUTO_CLOSE OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET AUTO_SHRINK OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET AUTO_UPDATE_STATISTICS ON 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET CURSOR_CLOSE_ON_COMMIT OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET CURSOR_DEFAULT  GLOBAL 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET CONCAT_NULL_YIELDS_NULL OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET NUMERIC_ROUNDABORT OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET QUOTED_IDENTIFIER OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET RECURSIVE_TRIGGERS OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET  DISABLE_BROKER 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET DATE_CORRELATION_OPTIMIZATION OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET TRUSTWORTHY OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET ALLOW_SNAPSHOT_ISOLATION OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET PARAMETERIZATION SIMPLE 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET READ_COMMITTED_SNAPSHOT OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET HONOR_BROKER_PRIORITY OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET RECOVERY FULL 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET  MULTI_USER 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET PAGE_VERIFY CHECKSUM  
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET DB_CHAINING OFF 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET TARGET_RECOVERY_TIME = 60 SECONDS 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET DELAYED_DURABILITY = DISABLED 
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET QUERY_STORE = OFF
--- GO
 
  USE [SecureDataStore]
  GO
 
--- ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
--- GO
 
--- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
--- GO
-
--- ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
--- GO
-
--- ALTER DATABASE [SecureDataStore] SET  READ_WRITE 
--- GO
 
 -- *********************************************************************************
 -- Create Pii Text Table
 -- *********************************************************************************
-CREATE TABLE [dbo].[PiiText](
+CREATE TABLE [dbo].[SecureText](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[Data] [varchar](100) NOT NULL,
 	[TimeStamp] [timestamp] NOT NULL,
-	[Type] [varchar](50) NOT NULL,)
+	[Type] [int] NOT NULL,)
 GO
 
 -- Create Pii Text indexes
@@ -171,11 +46,11 @@ GO
 -- *********************************************************************************
 -- Create Pii Date Table
 -- *********************************************************************************
-CREATE TABLE [dbo].[PiiDate](
+CREATE TABLE [dbo].[SecureDate](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[Data] [date] NOT NULL,
 	[TimeStamp] [timestamp] NOT NULL,
-	[Type] [varchar](50) NULL,)
+	[Type] [int] NULL,)
 GO
 
 -- Create Pii Date indexes
@@ -196,11 +71,11 @@ GO
 -- *********************************************************************************
 -- Create Pii DComplex ate Table
 -- *********************************************************************************
-CREATE TABLE [dbo].[PiiComplex](
+CREATE TABLE [dbo].[SecureComplex](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[Data] [varchar](max) NOT NULL,
 	[TimeStamp] [timestamp] NOT NULL,
-	[Type] [varchar](50) NULL,)
+	[Type] [int] NULL,)
 GO
 
 -- -- Create Pii Date indexes
@@ -216,3 +91,27 @@ CREATE UNIQUE CLUSTERED INDEX [idCluster] ON [dbo].[PiiComplex]
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
+
+
+CREATE TABLE [dbo].[PiiType](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	CONSTRAINT [PK_C\PiiType] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+	GO
+
+
+INSERT INTO PiiType([Name])
+Values
+	('SSN')
+	,('DOB')
+	,('DriversLicense')
+	,('DriversLicense')
+	,('CreditCardNumber')
+
+
+
+USE [master]
