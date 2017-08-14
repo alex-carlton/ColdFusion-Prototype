ed5aaee884c56c309396381fd0b0bf0e175f2c7f
Alex Carlton <ahcarlton@gmail.com>
2017-08-14 13:12:54 -0400

    Added Create Stored Procedures and Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ColdFusion-Prototype
+
+This repository will hold files used for the configuration of Data and Application Security Initiatives (DASI).
---
+++ StoredProcedures/CreateComplexTokenIfNotExists.sql
@@ -0,0 +1,14 @@
+USE ;
+GO
+
+CREATE PROCEDURE dbo.CreateComplexTokenIfNotExists
+   @TextValue varchar(100),
+   @Type varchar(50)
+AS
+
+   SET NOCOUNT ON;
+
+   SELECT
+   FROM
+   WHERE ;
+GO
