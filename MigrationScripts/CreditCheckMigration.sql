2e7c819dcaa2d1137548f357274173cf94a99f1c
JimmyMVegas <jamesaden@gmail.com>
2017-08-16 12:41:18 -0400

    Updated migration scripts

--- MigrationScripts/CreditCheckMigration.sql
+++ MigrationScripts/CreditCheckMigration.sql
@@ -2,32 +2,32 @@ USE [ApplicationDataStore]
 GO
 
 
-ALTER TABLE CreditCheck_Unmigrated ADD CreditRequestToken BIGINT NULL
+ALTER TABLE CreditChecks_Unmigrated ADD CreditRequestToken BIGINT NULL
 GO
 
 DECLARE @creditRequest varchar(max), @creditRequestToken BIGINT;
 
-DECLARE CreditCheckMigrationCursor CURSOR
-FOR SELECT CreditRequest, CreditRequestToken FROM CreditCheck_Unmigrated
+DECLARE CreditChecksMigrationCursor CURSOR
+FOR SELECT CreditRequest, CreditRequestToken FROM CreditChecks_Unmigrated
 FOR UPDATE OF CreditRequestToken
 
-OPEN CreditCheckMigrationCursor
-FETCH NEXT FROM CreditCheckMigrationCursor
+OPEN CreditChecksMigrationCursor
+FETCH NEXT FROM CreditChecksMigrationCursor
 INTO @creditRequest, @creditRequestToken
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-	INSERT INTO [SecureDataStore].dbo.SecureComplex (Data, Type)
+	INSERT INTO [SecureDataStore].dbo.SecureComplexes (SecureData, SecureTypeId)
 	SELECT @creditRequest, 5
 	
-	UPDATE CreditCheck_Unmigrated
+	UPDATE CreditChecks_Unmigrated
 	SET CreditRequestToken = SCOPE_IDENTITY()
-	WHERE CURRENT OF CreditCheckMigrationCursor
+	WHERE CURRENT OF CreditChecksMigrationCursor
 	
-	FETCH NEXT FROM CreditCheckMigrationCursor
+	FETCH NEXT FROM CreditChecksMigrationCursor
 	INTO @creditRequest, @creditRequestToken
 END
 
-ALTER TABLE CreditCheck_Unmigrated ALTER COLUMN CreditRequestToken BIGINT NOT NULL
+ALTER TABLE CreditChecks_Unmigrated ALTER COLUMN CreditRequestToken BIGINT NOT NULL
 
-ALTER TABLE CreditCheck_Unmigrated DROP COLUMN CreditRequest
+ALTER TABLE CreditChecks_Unmigrated DROP COLUMN CreditRequest
--- MigrationScripts/LoanApplicationMigration.sql
+++ MigrationScripts/LoanApplicationMigration.sql
@@ -1,44 +1,44 @@
 USE [ApplicationDataStore]
 GO
 
-ALTER TABLE LoanApplication_Unmigrated ADD DOBToken BIGINT NULL
-ALTER TABLE LoanApplication_Unmigrated ADD SSNToken BIGINT NULL
+ALTER TABLE LoanApplications_Unmigrated ADD DOBToken BIGINT NULL
+ALTER TABLE LoanApplications_Unmigrated ADD SSNToken BIGINT NULL
 GO
 
-INSERT INTO [SecureDataStore].dbo.SecureDate(Data, Type)
+INSERT INTO [SecureDataStore].dbo.SecureDates(SecureData, SecureTypeId)
 SELECT DOB, 2
-FROM LoanApplication_Unmigrated
+FROM LoanApplications_Unmigrated
 GROUP BY DOB
 EXCEPT
-SELECT Data, Type
-FROM [SecureDataStore].dbo.SecureDate
-WHERE Type = 2
+SELECT SecureData, SecureTypeId
+FROM [SecureDataStore].dbo.SecureDates
+WHERE SecureTypeId = 2
 
-INSERT INTO [SecureDataStore].dbo.SecureText(Data, Type)
+INSERT INTO [SecureDataStore].dbo.SecureTexts(SecureData, SecureTypeId)
 SELECT SSN, 1
-FROM LoanApplication_Unmigrated
+FROM LoanApplications_Unmigrated
 GROUP BY SSN
 EXCEPT
-SELECT Data, Type
-FROM [SecureDataStore].dbo.SecureText
-WHERE Type = 1
+SELECT SecureData, SecureTypeId
+FROM [SecureDataStore].dbo.SecureTexts
+WHERE SecureTypeId = 1
 GO
 
-UPDATE LoanApplication_Unmigrated
-SET DOBToken = sd.[Key]
-FROM [SecureDataStore].dbo.SecureDate sd
-WHERE sd.Data = LoanApplication_Unmigrated.DOB
-AND Type = 2
+UPDATE LoanApplications_Unmigrated
+SET DOBToken = sd.SecureDateId
+FROM [SecureDataStore].dbo.SecureDates sd
+WHERE sd.SecureData = LoanApplications_Unmigrated.DOB
+AND SecureTypeId = 2
 
-UPDATE LoanApplication_Unmigrated
-SET SSNToken = st.[Key]
-FROM [SecureDataStore].dbo.SecureText st
-WHERE st.Data = LoanApplication_Unmigrated.SSN
-AND Type = 1
+UPDATE LoanApplications_Unmigrated
+SET SSNToken = st.SecureTextId
+FROM [SecureDataStore].dbo.SecureTexts st
+WHERE st.SecureData = LoanApplications_Unmigrated.SSN
+AND SecureTypeId = 1
 GO
 
-ALTER TABLE LoanApplication_Unmigrated ALTER COLUMN DOBToken BIGINT NOT NULL
-ALTER TABLE LoanApplication_Unmigrated ALTER COLUMN SSNToken BIGINT NOT NULL
+ALTER TABLE LoanApplications_Unmigrated ALTER COLUMN DOBToken BIGINT NOT NULL
+ALTER TABLE LoanApplications_Unmigrated ALTER COLUMN SSNToken BIGINT NOT NULL
 
-ALTER TABLE LoanApplication_Unmigrated DROP COLUMN DOB
-ALTER TABLE LoanApplication_Unmigrated DROP COLUMN SSN
+ALTER TABLE LoanApplications_Unmigrated DROP COLUMN DOB
+ALTER TABLE LoanApplications_Unmigrated DROP COLUMN SSN
